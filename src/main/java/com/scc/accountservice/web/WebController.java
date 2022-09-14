19587038e4b1ed2e8193c00b9bd3db73283c3606
22050087 <22050087@myrp.edu.sg>
2022-09-14 19:17:23 +0800

    Added Login/Logout functionality
    Added CustomUserDetails.java
    Added CustomUserDetailsService.java
    Added FieldMatch.java
    Added FieldMatchValidator.java
    Added WebController.java
    Added WebSecurityConfig.java
    Added users.html

---
+++ src/main/java/com/scc/accountservice/web/WebController.java
@@ -0,0 +1,50 @@
+package com.scc.accountservice.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.scc.accountservice.model.User;
+import com.scc.accountservice.service.UserService;
+
+import java.util.List;
+
+@Controller
+public class WebController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping("")
+	public String viewHomePage() {
+		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String showRegistrationForm(Model model) {
+		model.addAttribute("user", new User());
+		
+		return "signup_form";
+	}
+	
+	@PostMapping("/process_register")
+	public String processRegister(User user) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+		
+		userService.saveUser(user);		
+		return "register_success";
+	}
+	
+	@GetMapping("/users")
+	public String listUsers(Model model) {
+		List<User> listUsers = userService.getAllUsers();
+		model.addAttribute("listUsers", listUsers);
+		
+		return "users";
+	}
+}
